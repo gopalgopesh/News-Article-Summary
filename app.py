6a69d0f7e916fb43233a902c69a9716d262b2987
gopalgopesh <39294458+gopalgopesh@users.noreply.github.com>
2020-11-16 13:23:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st 
-import nltk
-from textblob import TextBlob
 from newspaper import Article
-
-# url = 'https://edition.cnn.com/2020/08/02/tech/microsoft-tiktok/index.html'
 
 "## Enter your News url here"
 url = st.text_input("url")
